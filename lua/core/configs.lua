c1e915fb5db5d5006c3febe3b9f2b603129e2f36
faystos <fayst_@bk.ru>
2025-04-22 01:47:59 +0300

    add basic configuration

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -1,1 +1,17 @@
-print('configs')
+-- Line numbers
+vim.wo.number = true
+
+-- Indent setting
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Other
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+
+-- Mouse
+vim.opt.mouse = 'a'
+vim.opt.mousefocus = true
+
+
