97d6a72d41db87c2f03f761260b7b6d870abaf8f
faystos <fayst_@bk.ru>
2025-04-23 00:37:40 +0300

    add split keys mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -3,4 +3,17 @@ vim.g.mapleader = ' '
 
 -- Buffers
 vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+
+-- Split navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+
+-- Split window
+vim.keymap.set('n', '|', ':vsplit<CR>')
+vim.keymap.set('n', '\\', ':split<CR>')
+
+
 
