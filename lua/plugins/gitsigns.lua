77a4eabec0ba2100498c7f102112ca05c5049444
faystos <fayst_@bk.ru>
2025-05-12 20:40:17 +0300

    install plugin mason

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,7 @@
 return {
-  {
    "lewis6991/gitsigns.nvim",
+	 version = '1.0.0',
    config = function()
-      require('gitsigns').setup() 
+		require('gitsigns').setup() 
    end
-  }
-}
+} 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,10 @@
 return {
-	{'neovim/nvim-lspconfig'},
+
+		'neovim/nvim-lspconfig',
+		version = 'v1.0.0',
+		config = function()
+			local lspconfig = require('lspconfig')
+			lspconfig.lau_ls.setup{}
+		end
+
 }
