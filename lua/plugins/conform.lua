839b4a1021689f93c6cc661f75e5b2c97376adaf
faystos <fayst_@bk.ru>
2025-05-19 23:50:32 +0300

    feat: iinstall plugin conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		branch = "nvim-0.8",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+				},
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+}
